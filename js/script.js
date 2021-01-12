6fe28fb1e368e0f92a0afb2ab442f37ef8e0b259
famory1 <63998288+famory1@users.noreply.github.com>
2021-01-12 14:28:22 +0300

    213123123

--- js/script.js
+++ js/script.js
@@ -231,3 +231,26 @@ $(document).ready(function(){
 		event.preventDefault();
 	});
 });
+
+
+// Отправка форм
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
