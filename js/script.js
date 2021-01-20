eb2f6e8e0e828f6e66147d059a5ce67085c400f0
famory1 <63998288+famory1@users.noreply.github.com>
2021-01-20 11:51:57 +0300

    page speed

--- js/script.js
+++ js/script.js
@@ -357,3 +357,27 @@ $(document).ready(function() {
 	});
 
 });
+
+
+// Ленивая загрузка гугл карт
+let map_container = document.getElementById('contact__google-map');
+    let options_map = {
+        once: true,
+        passive: true,
+        capture: true
+    };
+    map_container.addEventListener('click', start_lazy_map, options_map);
+    map_container.addEventListener('mouseover', start_lazy_map, options_map);
+    map_container.addEventListener('touchstart', start_lazy_map, options_map);
+    map_container.addEventListener('touchmove', start_lazy_map, options_map);
+
+    let map_loaded = false;
+    function start_lazy_map() {
+        if (!map_loaded) {
+            let map_block = document.getElementById('google-maps');
+            map_loaded = true;
+            map_block.setAttribute('src', map_block.getAttribute('data-src'));
+            map_block.removeAttribute('data-src');
+            console.log('YMAP LOADED');
+        }
+    }
